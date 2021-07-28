7e35216b7a2b1758ba4717660e71dc63d4717129
lis154 <lis154@mail.ru>
2021-07-28 22:17:31 +0300

    Initial commit

---
+++ src/main/java/ru/lis154/dockerKafka/DockerKafkaApplication.java
@@ -0,0 +1,13 @@
+package ru.lis154.dockerKafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/lis154/dockerKafka/DockerKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.lis154.dockerKafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
